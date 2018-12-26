56d93d0244ed2a3641e698f522582b5850b745f9
pdaxrom <sashz@pdaxrom.org>
2018-12-26 13:04:43 +0400

    fixed irq

--- rtl/superio/simpleio.v
+++ rtl/superio/simpleio.v
@@ -61,53 +61,55 @@ module simpleio (
 			end
 		end
 	end
-	
-	always @ (cs or rw or clk) begin
-		if (rst) begin
-			leds <= 8'b11111111;
-			rgb1 <= 8'b111;
-			rgb2 <= 8'b111;
-			led7hi <= 0;
-			led7lo <= 0;
-			timer_mode <= 0;
-			timer_prescaler <= 0;
+
+	always @ (posedge clk) begin
+		if (rst) timer_mode[7] <= 0;
+		else if (cs && rw && clk) begin
+			case (AD[3:0])
+			4'b0000: DO <= ~leds;
+			4'b0001: DO <= led7hi;
+			4'b0010: DO <= led7lo;
+			4'b0011: begin
+				DO[6:4] <= ~rgb1;
+				DO[2:0] <= ~rgb2;
+				end
+			4'b0100: DO <= {switches, ~keys};
+			4'b1000: begin
+				DO <= timer_mode;
+				timer_mode[7] <= 0;
+				end
+			4'b1001: DO <= timer_mode[0]?timer_cnt[23:16]:timer_prescaler[23:16];
+			4'b1010: DO <= timer_mode[0]?timer_cnt[15:8]:timer_prescaler[15:8];
+			4'b1011: DO <= timer_mode[0]?timer_cnt[7:0]:timer_prescaler[7:0];
+			endcase
 		end else begin
 			if (timer_eq_flag) timer_mode[7] <= 1;
+		end
+	end
 
-				if (cs && rw && clk) begin
-					case (AD[3:0])
-					4'b0000: DO <= ~leds;
-					4'b0001: DO <= led7hi;
-					4'b0010: DO <= led7lo;
-					4'b0011: begin
-						DO[6:4] <= ~rgb1;
-						DO[2:0] <= ~rgb2;
-						end
-					4'b0100: DO <= {switches, ~keys};
-					4'b1000: begin
-						DO <= timer_mode;
-						timer_mode[7] <= 0;
-						end
-					4'b1001: DO <= timer_mode[0]?timer_cnt[23:16]:timer_prescaler[23:16];
-					4'b1010: DO <= timer_mode[0]?timer_cnt[15:8]:timer_prescaler[15:8];
-					4'b1011: DO <= timer_mode[0]?timer_cnt[7:0]:timer_prescaler[7:0];
-					endcase
+	always @(negedge clk) begin
+		if (rst) begin
+				leds <= 8'b11111111;
+				rgb1 <= 8'b111;
+				rgb2 <= 8'b111;
+				led7hi <= 0;
+				led7lo <= 0;
+				timer_mode[6:0] <= 0;
+				timer_prescaler <= 0;
+		end else if (cs && !rw && !clk) begin
+			case (AD[3:0])
+			4'b0000: leds <= ~DI;
+			4'b0001: led7hi <= DI;
+			4'b0010: led7lo <= DI;
+			4'b0011: begin
+				rgb1 <= ~DI[6:4];
+				rgb2 <= ~DI[2:0];
 				end
-				if (cs && !rw && !clk) begin
-					case (AD[3:0])
-					4'b0000: leds <= ~DI;
-					4'b0001: led7hi <= DI;
-					4'b0010: led7lo <= DI;
-					4'b0011: begin
-						rgb1 <= ~DI[6:4];
-						rgb2 <= ~DI[2:0];
-						end
-					4'b1000: timer_mode[6:0] <= DI[6:0];
-					4'b1001: timer_prescaler[23:16] <= DI;
-					4'b1010: timer_prescaler[15:8] <= DI;
-					4'b1011: timer_prescaler[7:0] <= DI;
-					endcase
-				end
+			4'b1000: timer_mode[6:0] <= DI[6:0];
+			4'b1001: timer_prescaler[23:16] <= DI;
+			4'b1010: timer_prescaler[15:8] <= DI;
+			4'b1011: timer_prescaler[7:0] <= DI;
+			endcase
 		end
 	end
 
