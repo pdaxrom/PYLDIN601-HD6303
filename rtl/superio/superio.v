a596d10be77eb4178428ced859e6192f88c11824
pdaxrom <sashz@pdaxrom.org>
2019-01-02 13:46:06 +0400

    ps2 keyboard initial

--- rtl/superio/superio.v
+++ rtl/superio/superio.v
@@ -9,8 +9,8 @@ module superio (
 	input	[7:0]	ADDR,
 	inout	[7:0]	DATA,
 
-	input			PS2CLK,
-	input			PS2DAT,
+	inout			PS2CLK,
+	inout			PS2DAT,
 
 	output	[1:0]	NSS,
 	output			MSCK,
@@ -161,7 +161,7 @@ module superio (
 		.CLK(audio_clk),
 		.RESET(sys_res)
 	);
-	wire spiio_cs = DS6 && (ADDR[4] == 1'b0); // $E6C0
+	wire spiio_cs = DS6 && (ADDR[4:3] == 2'b00); // $E6C0
 	wire [7:0] spiio_dout;
 	spiio spi_impl(
 		.clk(E),
@@ -181,15 +181,32 @@ module superio (
 		.pout({BLANK, RS})
 	);
 
+	wire ps2io_cs = DS6 && (ADDR[4:3] == 2'b10); // $E6D0
+	wire [7:0] ps2io_dout;
+	wire ps2io_irq;
+	ps2io ps2io_impl(
+		.clk(E),
+		.rst(sys_res),
+		.AD(ADDR[2:0]),
+		.DI(DATA),
+		.DO(ps2io_dout),
+		.rw(RW),
+		.cs(ps2io_cs),
+		.irq(ps2io_irq),
+		.ps2clk(PS2CLK),
+		.ps2dat(PS2DAT)
+	);
+
 	wire [7:0] DOUT = simpleio_cs ? simpleio_dout	:
 					  psg_cs	  ? psg_dout 		:
 					  spiio_cs	  ? spiio_dout		:
+					  ps2io_cs	  ? ps2io_dout		:
 					  vpu_cs	  ? vpu_dout		:
 					  8'b10100101;
 	
 	assign DATA = (RW & !EXTCS) ? DOUT : 8'bZZZZZZZZ;
 	
-	assign IRQ[0] = !(simpleio_irq | vpu_irq);
+	assign IRQ[0] = !(simpleio_irq | vpu_irq | ps2io_irq);
 	assign IRQ[1] = 1;
 	assign NMI = 1;
 endmodule
