b2808022b31a56fafba91073aefbb39ff39e496f
pdaxrom <sashz@pdaxrom.org>
2019-01-23 16:06:02 +0800

    offset and checksum

--- SW/ASM/SOFT/patcher.c
+++ SW/ASM/SOFT/patcher.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
 
 unsigned char mem[65536];
 
@@ -46,21 +47,37 @@ int main(int argc, char *argv[])
     uint32_t addr;
     uint32_t cmd, byte;
     char type;
+    char buff[256];
+    uint32_t offset = 0x100;
+    uint32_t csum_addr = 0;
+    int	csum_enable = 0;
 
-    while (fscanf(f, "ADDR=%04X (%02X %02X %c)\n", &addr, &cmd, &byte, &type) > 0) {
+    while (fgets(buff, sizeof(buff), f)) {
+	type = 0;
+	if (strchr(buff, ';')) {
+	    continue;
+	} else if (!strncmp(buff, "OFFSET=", 7)) {
+	    sscanf(buff, "OFFSET=%04X\n", &offset);
+	    continue;
+	} else if (!strncmp(buff, "CHECKSUM=", 9)) {
+	    sscanf(buff, "CHECKSUM=%04X\n", &csum_addr);
+	    csum_enable = 1;
+	    continue;
+	}
+	sscanf(buff, "ADDR=%04X (%02X %02X %c)\n", &addr, &cmd, &byte, &type);
 	if (type == 'X') {
 	    //printf("%04X %02X %02X\n", addr, cmd, byte);
-	    if (mem[addr + 0 - 0x100] != cmd ||
-		mem[addr + 1 - 0x100] != byte) {
+	    if (mem[addr + 0 - offset] != cmd ||
+		mem[addr + 1 - offset] != byte) {
 		fprintf(stderr, "Skip patch ADDR=%04X MEM=%04X\n", addr, byte);
 	    } else {
-		mem[addr + 1 - 0x100] += 0x28;
+		mem[addr + 1 - offset] += 0x28;
 	    }
 	} else if (type == 'P') {
-	    if (mem[addr + 0 - 0x100] != cmd) {
+	    if (mem[addr + 0 - offset] != cmd) {
 		fprintf(stderr, "Skip patch ADDR=%04X\n", addr);
 	    } else {
-		mem[addr + 0 - 0x100] = byte;
+		mem[addr + 0 - offset] = byte;
 	    }
 	} else {
 	    break;
@@ -68,6 +85,24 @@ int main(int argc, char *argv[])
     }
 
     fclose(f);
+
+    uint8_t csum;
+
+    if (csum_enable) {
+	csum = 0;
+	mem[csum_addr - offset] = 0xFF;
+	for (ptr = 0; ptr < len; ptr++) {
+	    csum += mem[ptr];
+	}
+	mem[csum_addr - offset] = 0xFF ^ csum;
+    }
+
+    csum = 0;
+    for (ptr = 0; ptr < len; ptr++) {
+	csum += mem[ptr];
+    }
+    fprintf(stderr, "Checksum = %02X\n", csum);
+
 
     f = fopen(argv[3], "wb");
     if (!f) {
